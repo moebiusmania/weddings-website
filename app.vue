4b1e02aaec0bab942f222ba05f58cbeab84a45ca
Salvatore Laisa <moebiusmania@gmail.com>
2022-10-09 18:00:53 +0000

    chore: bootstrapping the template

--- app.vue
+++ app.vue
@@ -1,5 +1,67 @@
+<script lang="ts" setup>
+useHead({
+  title: "Lucia & Francesco",
+  meta: [
+    {
+      name: 'description',
+      content: "Matrimobio Lucia & Francesco"
+    }
+  ],
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css?family=Sacramento'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'css/animate.css',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'css/icomoon.css',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'css/bootstrap.css',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'css/magnific-popup.css',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'css/owl.carousel.min.css',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'css/owl.theme.default.min.css',
+    },
+  ]
+})
+</script>
+
+<style lang="scss">
+@import "./assets/sass/overrides.scss";
+@import "./assets/sass/bootstrap.scss";
+@import "./assets/sass/style.scss";
+</style>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div id="page">
+    <Nav />
+    <Header />
+    <Couple />
+    <Event />
+    <Story />
+    <!-- <Gallery /> -->
+    <Counter />
+    <Testimonial />
+    <Services />
+    <Form />
+    <Footer />
   </div>
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer id="fh5co-footer" role="contentinfo">
+    <div class="container">
+
+      <div class="row copyright">
+        <div class="col-md-12 text-center">
+          <p>
+            <small class="block">&copy; 2022. Some Rights Reserved.</small>
+            <small class="block">Developed by <a href="https://salvatorelaisa.blog/" target="_blank"
+                rel="noopener nofollow">Salvatore Laisa</a>, source and <a
+                href="https://github.com/moebiusmania/weddings-website" target="_blank"
+                rel="noopener nofollow">credits</a>.
+              <!-- <span>Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></span> -->
+            </small>
+          </p>
+          <!-- <p>
+          <ul class="fh5co-social-icons">
+            <li><a href="#"><i class="icon-twitter"></i></a></li>
+            <li><a href="#"><i class="icon-facebook"></i></a></li>
+            <li><a href="#"><i class="icon-instagram"></i></a></li>
+          </ul>
+          </p> -->
+        </div>
+      </div>
+
+    </div>
+  </footer>
+</template>
