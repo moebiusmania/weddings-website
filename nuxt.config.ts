1ea3523935d42880e9b96210eda588afd5a78040
Salvatore Laisa <moebiusmania@gmail.com>
2022-10-09 23:14:16 +0000

    fix: config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ const isProd = (): boolean => process.env.NODE_ENV === 'production'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  baseURL: isProd ? "/weddings-website/" : "/",
-  buildAssetsDir: "."
+  app: {
+    baseURL: isProd() ? "/weddings-website/" : "/",
+  }
 })
