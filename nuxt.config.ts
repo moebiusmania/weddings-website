b9e7a5fac1fd4de8c17e65b01aaa0c2aba778430
moebiusmania <moebiusmania@gmail.com>
2022-11-14 21:33:04 +0100

    fix: remove subfolder conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ const isProd = (): boolean => process.env.NODE_ENV === 'production'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
-    baseURL: isProd() ? "/weddings-website/" : "/",
-    cdnURL: isProd() ? "https://moebiusmania.github.io/weddings-website" : "",
+    // baseURL: isProd() ? "/weddings-website/" : "/",
+    // cdnURL: isProd() ? "https://moebiusmania.github.io/weddings-website" : "",
   }
 })
